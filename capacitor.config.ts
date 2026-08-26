9f3734dcbac289f82ccc7d76947a0bd65d717f0d
Fernando Cerqueira <fernandofznn@gmail.com>
2026-08-26 12:36:53 -0300

    IPA build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.fernando.shape76',
   appName: 'Shape 76',
-  webDir: '.next'
+  webDir: "out",
 };
 
 export default config;
